83727e6494f3a834c65c4616127df4fc19210bf9
Pht7 <59353381+Pht7@users.noreply.github.com>
2020-09-25 22:42:20 -0400

    Updated data base to correct information, added long + latitude

--- addData.py
+++ addData.py
@@ -1,12 +1,34 @@
+import requests
+import json
 from firebase import firebase
+from geopy.distance import great_circle
+
+
+
+
+send_url = "http://api.ipstack.com/check?access_key=6a554c654da3f5d634ab99a2bedae475"
+geo_req = requests.get(send_url)
+geo_json = json.loads(geo_req.text)
+latitude = geo_json['latitude']
+longitude = geo_json['longitude']
+city = geo_json['city']
+
 
 firebase = firebase.FirebaseApplication('https://close-draw.firebaseio.com', None)
 data = {
-    'Name':'Sam M',
-    'SSN':'219',
-    'Email':'poop'
+    'Longitude':longitude,
+    'Latitude':latitude,
+    'City':city,
+    'Time':'WIP',
+    'Picture Location':'PUTGOOGLECLOUDSPACE',
+    'Caption':'HEREWITHMYGIRLS'
 }
 
-result = firebase.post('/close-draw/Student', data)
+result = firebase.post('/close-draw/TestDB', data)
 
 print(result)
+
+coords_1 = (40.7428, -74.1771)
+coords_2 = (latitude, longitude)
+
+print(great_circle(coords_1, coords_2).miles)
