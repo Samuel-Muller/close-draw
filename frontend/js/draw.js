cfeeb9e4c8da339997e8ea6dd2d3e45b37f80ced
Evan J. Markowitz <evan.markowitz99@gmail.com>
2020-09-27 19:51:17 +0000

    Fix brandig and add spacing between posts

--- frontend/js/draw.js
+++ frontend/js/draw.js
@@ -2,10 +2,27 @@
  * Licensed under MIT
  */
 
+var firebaseConfig = {
+  apiKey: "AIzaSyCCm8uJxxDWemcC8FDG_x3ZX6EgOrmyk2g",
+  authDomain: "close-draw.firebaseapp.com",
+  databaseURL: "https://close-draw.firebaseio.com",
+  projectId: "close-draw",
+  storageBucket: "close-draw.appspot.com",
+  messagingSenderId: "1021757931194",
+  appId: "1:1021757931194:web:16254c5da128139f66cc83",
+  measurementId: "G-JY2FYVSB2R"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+var storage = firebase.storage();
+
+var canvas = document.getElementById('canvas');
 var context = document.getElementById('canvas').getContext("2d");
 var penColor = "black";
 var penSize = $('.pen-width').val();
-var paint; 
+var paint;
 
 var brushStrokes = [];
 var strokeStarts = [];
@@ -77,7 +94,7 @@ function undo() {
 function redraw() {
   context.clearRect(0, 0, context.canvas.width, context.canvas.height);
   context.lineJoin = "round";
-  for (var i = 0; i < brushStrokes.length; i++) {		
+  for (var i = 0; i < brushStrokes.length; i++) {
     context.beginPath();
     context.strokeStyle = brushStrokes[i].color;
     context.lineWidth = brushStrokes[i].size;
@@ -123,6 +140,57 @@ sendDialog.querySelector('.close').addEventListener('click', function() {
 });
 sendDialog.querySelector('.confirm').addEventListener('click', function() {
   //clear();
-  
+  submit();
   sendDialog.close();
 });
+
+function submit() {
+  function locSuccess(position) {
+    const latitude  = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    var url = new URL("https://evanjmarkowitz.com/loodle/upload")
+    var caption = $("#caption").val();
+    var drawing = canvas.toDataURL();
+    var timestamp = Number(new Date());
+    var storageRef = firebase.storage().ref(timestamp.toString());
+
+    storageRef.putString(drawing, 'data_url').then(function(snapshot) {
+      snapshot.ref.getDownloadURL().then(function(download) {
+        const data = { url: download, caption: caption, lat: latitude, lon: longitude };
+
+        fetch('https://evanjmarkowitz.com/loodle/upload', {
+          method: 'POST', // or 'PUT'
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(data),
+        })
+        .then(response => response.json())
+        .then(data => {
+           console.log('Success:', data);
+        })
+        .catch((error) => {
+          console.error('Error:', error)
+        });
+      });
+    });
+
+    url.search = new URLSearchParams(params).toString();
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      for (let drawing of data) {
+        elem = drawingToCard(drawing);
+        document.getElementById("drawings").append(elem);
+      }
+    });
+  }
+  function locError() {
+    /* pass */
+  }
+  if (!navigator.geolocation) {
+    /* shh */
+  } else {
+    navigator.geolocation.getCurrentPosition(locSuccess, locError);
+  }
+}
